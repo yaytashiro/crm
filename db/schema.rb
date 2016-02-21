1311cd5a4e463207b41adc80bbc140a5a597d34c
田代やよい <o.841.312@gmail.com>
2016-02-21 14:14:53 +0900

    app/controller/application_controller.rbの書き方はRuby5になるとつかえないので注意が必要。あとは自分で調べてね！

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221021103) do
+ActiveRecord::Schema.define(version: 20160221045929) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",       null: false
@@ -46,6 +46,9 @@ ActiveRecord::Schema.define(version: 20160221021103) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "ToUsers"
+    t.string   "family_name"
+    t.string   "given_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
